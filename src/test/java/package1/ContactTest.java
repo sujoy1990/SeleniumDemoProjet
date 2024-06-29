f2ea895312429eadf33d8117da1d36f90895379c
sujoy126 <sujoyoffice126@gmail.com>
2024-06-29 16:10:19 +0530

    First Push all the code.

---
+++ src/test/java/package1/ContactTest.java
@@ -0,0 +1,29 @@
+package package1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createContactTest() {
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
+		
+		
+		System.out.println("Excute createContactTest");
+	}
+	
+	@Test
+	public void modifyContactTest() {
+		System.out.println("Excute modifyContactTest");
+	}
+
+}
---
+++ src/test/java/package2/OrgTest.java
@@ -0,0 +1,17 @@
+package package2;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createOrgTest() {
+		System.out.println("Excute createOrgTest");
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("Excute modifyOrgTest");
+	}
+
+}
