6bc92e49a5c081f10fad9a179ad8bde212d6dbc7
sujoy126 <sujoyoffice126@gmail.com>
2024-07-01 15:56:32 +0530

    org Delete Added

--- src/test/java/package2/OrgTest.java
+++ src/test/java/package2/OrgTest.java
@@ -7,11 +7,26 @@ public class OrgTest {
 	@Test
 	public void createOrgTest() {
 		System.out.println("Excute createOrgTest");
+		
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 	}
 	
 	@Test
 	public void modifyOrgTest() {
 		System.out.println("Excute modifyOrgTest");
+	}
+	
+	@Test
+	public void deleteOrgTest() {
+		System.out.println("Excute deleteOrgTest");
 	}
 
 }
